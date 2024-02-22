9ac5d035963340acaef4dd7c742430caf05c95f0
paulooctavioINTELI <124174379+paulooctavioINTELI@users.noreply.github.com>
2024-02-21 21:37:42 -0300

    ajust imports on index

--- index.js
+++ index.js
@@ -1,27 +1,57 @@
 const app = require("express")();
-const { scrapeDynamicContent } = require('./globo_leiloes'); 
+const puppeteer = require('puppeteer');
+const { scrapeDynamicContent } = require("./globo_leiloes");
 
 app.get("/", async (req, res) => {
+  const categorias = [
+    ...new Set([
+      "Terreno",
+      "Lote",
+      "Vaga de Garagem",
+      "Casa",
+      "Sobrado",
+      "Apartamento",
+      "Cobertura",
+      "Fazenda",
+      "Gleba",
+      "Chácara",
+      "Sítio",
+      "Sala",
+      "Escritório",
+      "Galpão",
+    ]),
+  ];
+  const baseUrl =
+    "https://globoleiloes.com.br/leiloes/residenciais/todos-os-residenciais/todos-os-estados/todas-as-cidades/";
 
-  const categorias = [...new Set([
-    "Terreno", "Lote", "Vaga de Garagem", "Casa", "Sobrado",
-    "Apartamento", "Cobertura", "Fazenda", "Gleba",
-    "Chácara", "Sítio", "Sala", "Escritório", "Galpão"
-  ])];
-  const baseUrl = "https://globoleiloes.com.br/leiloes/residenciais/todos-os-residenciais/todos-os-estados/todas-as-cidades/";
-  
   try {
     console.time("ScrapingExecutionTime");
 
     // const result = await scrapeDynamicContent(baseUrl, categorias);
     // res.status(200).json(result)
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      timeout: 60000,
+      headless: true, // Garante que está em modo headless
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-accelerated-2d-canvas",
+        "--disable-gpu",
+        "--disable-images",
+        // "--single-process",
+        // "--no-zygote",
+      ],
+      executablePath:
+        process.env.NODE_ENV === "production"
+          ? process.env.PUPPETEER_EXECUTABLE_PATH
+          : puppeteer.executablePath(),
+    });
     let page = await browser.newPage();
     await page.goto("https://www.google.com");
     res.send(await page.title());
     console.timeEnd("ScrapingExecutionTime");
-
   } catch (err) {
     console.error(err);
     return null;
