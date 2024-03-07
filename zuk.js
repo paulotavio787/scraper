e54513262f4af5c5cd158847ac717e5adb13f9ab
paulooctavioINTELI <124174379+paulooctavioINTELI@users.noreply.github.com>
2024-03-07 13:08:35 -0300

    zuk

--- zuk.js
+++ zuk.js
@@ -38,22 +38,20 @@ async function scrapePage(url, retryCount = 0) {
   try {
     await page.goto(url, { waitUntil: "domcontentloaded", timeout: 60000 });
 
-    // const items = await page.evaluate(() => {
-    //     return Array.from(document.querySelectorAll('.card-property')).map(card => {
-    //       const url = card.querySelector('.card-property-image-wrapper a') ? card.querySelector('.card-property-image-wrapper a').href : 'No URL';
-    //       const imageUrl = card.querySelector('.card-property-image-wrapper img') ? card.querySelector('.card-property-image-wrapper img').src : 'No Image URL';
-    //       const description = card.querySelector('.card-property-proposta-open') ? card.querySelector('.card-property-proposta-open').innerText.trim() : 'No Description';
-    //       const address = card.querySelector('.card-property-address span') ? card.querySelector('.card-property-address span').innerText.trim() : 'No Address';
-    //       const area = card.querySelector('.card-property-info-label') ? card.querySelector('.card-property-info-label').innerText.trim() : 'No Area';
-    //       const value = card.querySelector('.card-property-price-value') ? card.querySelector('.card-property-price-value').innerText.trim() : 'No Value';
+    const items = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll('.card-property')).map(card => {
+          const url = card.querySelector('.card-property-image-wrapper a') ? card.querySelector('.card-property-image-wrapper a').href : 'No URL';
+          const imageUrl = card.querySelector('.card-property-image-wrapper img') ? card.querySelector('.card-property-image-wrapper img').src : 'No Image URL';
+          const description = card.querySelector('.card-property-proposta-open') ? card.querySelector('.card-property-proposta-open').innerText.trim() : 'No Description';
+          const address = card.querySelector('.card-property-address span') ? card.querySelector('.card-property-address span').innerText.trim() : 'No Address';
+          const area = card.querySelector('.card-property-info-label') ? card.querySelector('.card-property-info-label').innerText.trim() : 'No Area';
+          const value = card.querySelector('.card-property-price-value') ? card.querySelector('.card-property-price-value').innerText.trim() : 'No Value';
       
-    //       return { url, imageUrl, description, address, area, value };
-    //     });
-    //   });
-
-    const title = await page.title()
+          return { url, imageUrl, description, address, area, value };
+        });
+      });
     
-    return title;
+    return items;
   } catch (e) {
     console.error(`Error loading page: ${e.message}. Retry ${retryCount}`);
     if (retryCount < 3) {
@@ -70,19 +68,19 @@ async function scrapeAllItems(baseUrl, itemsPerPage = 30) {
   await setupBrowser();
 
   const totalItems = await getTotalItems(baseUrl);
-  // const numPages = Math.ceil(totalItems / itemsPerPage);
-  // const allItems = [];
+  const numPages = Math.ceil(totalItems / itemsPerPage);
+  const allItems = [];
 
-  // for (let i = 0; i < numPages; i++) {
-  //   const start = i * itemsPerPage;
-  //   const url = `${baseUrl}?start=${start}`;
-  //   const items = await scrapePage(url);
-  //   allItems.push(...items);
-  // }
+  for (let i = 0; i < numPages; i++) {
+    const start = i * itemsPerPage;
+    const url = `${baseUrl}?start=${start}`;
+    const items = await scrapePage(url);
+    allItems.push(...items);
+  }
 
   await page.close();
   await browser.close();
-  return `${totalItems}`;
+  return allItems;
 }
 
 const baseUrl = "https://www.portalzuk.com.br/leilao-de-imoveis";
